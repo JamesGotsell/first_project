671dd483dfcea636be660ed9883b6297bea4512b
JamesGotsell <j.e.gotsell@gmail.com>
2015-04-13 15:26:56 +0100

    migrated dbs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413140833) do
+ActiveRecord::Schema.define(version: 20150413142057) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "song_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.text     "song"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
