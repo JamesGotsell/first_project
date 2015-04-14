9b7e4df6f09605ba7556175561fc9b73b6426641
JamesGotsell <j.e.gotsell@gmail.com>
2015-04-14 14:10:21 +0100

    sorted out the image tag in user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,8 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
+
+  # def user_params
+  #   params.require(:user).permit( :name, :picture, :age, :bio )
+  # end
 end
