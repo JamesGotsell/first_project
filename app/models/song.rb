02d735a66645ac4061e0eee3e849924795eba9a3
JamesGotsell <j.e.gotsell@gmail.com>
2015-04-15 11:49:52 +0100

    added picture to song params

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,4 +6,5 @@ class Song < ActiveRecord::Base
       too_long:"%{count} characters is the maximum allowed" }
 
   mount_uploader :song, AudioUploader
+  mount_uploader :picture, PictureUploader
 end
