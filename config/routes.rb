c7de21badefab179a2aecbcfe6862f80ed412ac0
JamesGotsell <j.e.gotsell@gmail.com>
2015-04-16 22:11:23 +0100

    config the roots to home page...using high_voltage

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   resources :comments, except: [:show]
   resources :songs
   devise_for :users
-  devise_scope :user do 
-    root "devise/sessions#new"
-  end
+  # devise_scope :user do 
+  #   root "devise/sessions#new"
+  # end
 
+  HighVoltage.configure do |config|
+    config.home_page = 'home'
+  end
   
   # root "users#index"
   resources :users, only: [:index,:show]
