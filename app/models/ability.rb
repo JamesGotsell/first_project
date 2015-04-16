e78b1388f3ca99efb75377fd92ec468897dc5de1
JamesGotsell <j.e.gotsell@gmail.com>
2015-04-16 23:00:49 +0100

    cleaned up the code

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,39 +1,18 @@
 class Ability
   include CanCan::Ability
-
   def initialize(user)
-      user ||= User.new # guest user (not logged in)
-      # if user.admin?
-      #   can :manage, :all
-      # else
-      #   can :read, :all
-      # end
-
-      if user.role == "admin"
-        can :manage, :all 
-        can :destroy, Comment
-      # delete comments 
-      # delete songs 
-      # edit comments 
-      # i think that i've got this ability because the admin can manage all 
-
+    user ||= User.new 
+    if user.role == "admin"
+      can :manage, :all 
+      can :destroy, Comment
     else user.role == "user"
-        # add comments 
-        # add songs 
-        #delete song 
-
-        # can :read, User 
-
-        can :read, :all
-        can :update, Song, user_id: user.id 
-        can :update, Comment, user_id: user.id
-        can :create, Song, user_id: user.id
-        can :create, Comment do |comment|
-          comment.song.user != user.id
-        end
-
-        # can :manage, User, user_id: user.id 
+      can :read, :all
+      can :update, Song, user_id: user.id 
+      can :update, Comment, user_id: user.id
+      can :create, Song, user_id: user.id
+      can :create, Comment do |comment|
+        comment.song.user != user.id
       end
     end
-
   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-
   resources :comments, except: [:show]
   resources :songs
   devise_for :users
   # devise_scope :user do 
   #   root "devise/sessions#new"
   # end
-
   HighVoltage.configure do |config|
     config.home_page = 'home'
   end
-  
   # root "users#index"
   resources :users, only: [:index,:show]
   #root to: "registrations#new"
