417f23ca4f38b92bbae25326a490dd56b7944983
JamesGotsell <j.e.gotsell@gmail.com>
2015-04-16 09:39:22 +0100

    added user/admin abilities within cancancan abilitiy model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,16 @@ class Ability
 
   def initialize(user)
       user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      else
-        can :read, :all
+      # if user.admin?
+      #   can :manage, :all
+      # else
+      #   can :read, :all
+      # end
+
+      if user.role == "Admin"
+        can :manage, :all 
+      else user.role == "User"
+        can :read, User
+        can :manage, User, user_id: user.id 
       end
-   
-  end
-end
+    end
